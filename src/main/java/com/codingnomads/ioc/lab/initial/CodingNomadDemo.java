d06f1160b80fd362bfb6ef8241d0905815179e01
atck3r <androidmini92@gmail.com>
2025-10-06 18:36:06 +0300

    Done Ex

--- src/main/java/com/codingnomads/ioc/lab/initial/CodingNomadDemo.java
+++ src/main/java/com/codingnomads/ioc/lab/initial/CodingNomadDemo.java
@@ -3,15 +3,18 @@ package com.codingnomads.ioc.lab.initial;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication(exclude = {
         DataSourceAutoConfiguration.class
 })
 public class CodingNomadDemo {
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(
-                CodingNomadConfiguration.class
+//        ApplicationContext context = new AnnotationConfigApplicationContext(
+//                CodingNomadConfiguration.class
+//        );
+        ApplicationContext context = new ClassPathXmlApplicationContext(
+                "xml-config/codingnomad_configuration.xml"
         );
         CodingNomad codingNomad = context.getBean(CodingNomad.class);
         System.out.println(codingNomad.createAwesomeSoftware());
